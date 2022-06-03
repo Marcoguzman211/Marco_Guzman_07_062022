df1a7ee2a86f8023d0a6580958c249565c4b0564
Marco Guzman <62027255+Marcoguzman211@users.noreply.github.com>
2022-06-03 23:07:48 +0200

    dropdown css et html

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+/* 
+document.getElementById("holahola").addEventListener("click", () => {
+    document.querySelector(".menu").classList.toggle("hidden");
+}) */
+
+const toggleListBox = (listboxList, chevron ) => {
+    if (listboxList.classList.contains('hidden')) {
+        listboxList.classList.remove('hidden')
+        listboxList.classList.add('show')
+        chevron.classList.add('fa-chevron-up') 
+        chevron.classList.remove('fa-chevron-down') 
+      } else {
+        listboxList.classList.add('hidden')
+        listboxList.classList.remove('show')
+        chevron.classList.remove('fa-chevron-up') 
+        chevron.classList.add('fa-chevron-down') 
+      }
+}
+// Listboxs opening with chevron event 
+document.querySelectorAll('.listbox-controls .chevron').forEach(element => {
+    element.addEventListener('click', () => {
+      const parentElement = element.closest('.listbox'); 
+      const listboxList = parentElement.querySelector('.listbox-list');
+      const chevron = parentElement.querySelector('.chevron');
+      toggleListBox(listboxList, chevron);
+    });
+  });
