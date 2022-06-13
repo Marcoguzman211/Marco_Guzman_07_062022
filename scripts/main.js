6656976d3caa00b8c0d315013bb601cabb6b91c4
Marco Guzman <62027255+Marcoguzman211@users.noreply.github.com>
2022-06-13 16:14:55 +0200

    Fix html and css

--- scripts/main.js
+++ scripts/main.js
@@ -4,42 +4,42 @@ document.getElementById("holahola").addEventListener("click", () => {
 }) */
 
 const toggleListBox = ($listboxList, $chevron ) => {
-    if ($listboxList.classList.contains('hidden')) {
-        $listboxList.classList.remove('hidden')
-        $listboxList.classList.add('show')
-        $chevron.classList.add('fa-chevron-up') 
-        $chevron.classList.remove('fa-chevron-down') 
+    if ($listboxList.classList.contains("hidden")) {
+        $listboxList.classList.remove("hidden");
+        $listboxList.classList.add("show");
+        $chevron.classList.add("fa-chevron-up"); 
+        $chevron.classList.remove("fa-chevron-down"); 
       } else {
-        $listboxList.classList.add('hidden')
-        $listboxList.classList.remove('show')
-        $chevron.classList.remove('fa-chevron-up') 
-        $chevron.classList.add('fa-chevron-down') 
+        $listboxList.classList.add("hidden");
+        $listboxList.classList.remove("show");
+        $chevron.classList.remove("fa-chevron-up"); 
+        $chevron.classList.add("fa-chevron-down"); 
       }
-}
+};
 
 const openOnInputListbox = ($listboxList, $chevron) => {
-    $listboxList.classList.remove('hidden')
-    $listboxList.classList.add('show')
-    $chevron.classList.add('fa-chevron-up') 
-    $chevron.classList.remove('fa-chevron-down') 
-  }
+    $listboxList.classList.remove("hidden");
+    $listboxList.classList.add("show");
+    $chevron.classList.add("fa-chevron-up"); 
+    $chevron.classList.remove("fa-chevron-down"); 
+  };
 
 // Listboxs opening with chevron event 
-document.querySelectorAll('.listbox-controls .chevron').forEach(element => {
-    element.addEventListener('click', () => {
-      const parentElement = element.closest('.listbox'); 
-      const listboxList = parentElement.querySelector('.listbox-list');
-      const chevron = parentElement.querySelector('.chevron');
+document.querySelectorAll(".listbox-controls .chevron").forEach(element => {
+    element.addEventListener("click", () => {
+      const parentElement = element.closest(".listbox"); 
+      const listboxList = parentElement.querySelector(".listbox-list");
+      const chevron = parentElement.querySelector(".chevron");
       toggleListBox(listboxList, chevron);
     });
   });
 
   // Listboxs opening on input event
-document.querySelectorAll('.listbox-input').forEach(element => {
-    element.addEventListener('click', () => {
-      const parentElement = element.closest('.listbox'); 
-      const listboxList = parentElement.querySelector('.listbox-list');
-      const chevron = parentElement.querySelector('.chevron');
-      openOnInputListbox(listboxList, chevron)
-    })
-  })
+document.querySelectorAll(".listbox-input").forEach(element => {
+    element.addEventListener("click", () => {
+      const parentElement = element.closest(".listbox"); 
+      const listboxList = parentElement.querySelector(".listbox-list");
+      const chevron = parentElement.querySelector(".chevron");
+      openOnInputListbox(listboxList, chevron);
+    });
+  });
