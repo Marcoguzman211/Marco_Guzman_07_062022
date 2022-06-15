4d312dde1118e9405a51c9626c52fc213cd9891f
Marco Guzman <62027255+Marcoguzman211@users.noreply.github.com>
2022-06-15 11:36:29 +0200

    Listboxslist

--- scripts/main.js
+++ scripts/main.js
@@ -2,27 +2,24 @@
 document.getElementById("holahola").addEventListener("click", () => {
     document.querySelector(".menu").classList.toggle("hidden");
 }) */
+ import { toggleListBox, openOnInputListbox, getIngredients, getAppliances, getUstensils, createListboxsLists} from "./listboxs.js";
+ import {recipes} from "./data/recipes.js";
 
-const toggleListBox = ($listboxList, $chevron ) => {
-    if ($listboxList.classList.contains("hidden")) {
-        $listboxList.classList.remove("hidden");
-        $listboxList.classList.add("show");
-        $chevron.classList.add("fa-chevron-up"); 
-        $chevron.classList.remove("fa-chevron-down"); 
-      } else {
-        $listboxList.classList.add("hidden");
-        $listboxList.classList.remove("show");
-        $chevron.classList.remove("fa-chevron-up"); 
-        $chevron.classList.add("fa-chevron-down"); 
-      }
-};
+const tags = [];
+const ingredients = getIngredients(recipes);
+const ustensils = getUstensils(recipes);
+const appliances = getAppliances(recipes);
 
-const openOnInputListbox = ($listboxList, $chevron) => {
-    $listboxList.classList.remove("hidden");
-    $listboxList.classList.add("show");
-    $chevron.classList.add("fa-chevron-up"); 
-    $chevron.classList.remove("fa-chevron-down"); 
-  };
+// Listboxs lists nodes :
+const listboxIngredientsList = document.getElementById("ingredients-list");
+const listboxAppliancesList = document.getElementById("appliances-list");
+const listboxUstensilsList = document.getElementById("ustensils-list");
+
+// Create listboxs lists on load :
+createListboxsLists(recipes, getIngredients, listboxIngredientsList, "ingredients", tags);
+createListboxsLists(recipes, getAppliances, listboxAppliancesList, "appliances", tags); 
+createListboxsLists(recipes, getUstensils, listboxUstensilsList, "ustensils", tags); 
+
 
 // Listboxs opening with chevron event 
 document.querySelectorAll(".listbox-controls .chevron").forEach(element => {
